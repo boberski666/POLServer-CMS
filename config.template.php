3e7f146b600749d481b9382251708aa479f80c78
Boberski <boberski@torchwood.pl>
2017-06-07 22:56:00 +0200

    Project import - first working version!

---
+++ config.template.php
@@ -0,0 +1,11 @@
+<?php
+/*Database*/
+define("DB_SERVER", "localhost");
+define("DB_PORT", "3306");
+define("DB_NAME", "");
+define("DB_USER","");
+define("DB_PASS","");
+
+/*Character Tables*/
+define("TBL_CHARS", "characters");
+define("TBL_CHARS_LAYERS", "characters_layers");
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Ultima Online</title>
+<style>
+
+	/*Custom CSS Overrides*/
+	body {
+		font-family: 'Lato', sans-serif !important;
+		background-color: transparent !important;
+	}
+	* { margin: 0; padding: 0; }
+	html {
+		background: url(images/page.jpg) no-repeat center center fixed;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+	}
+</style>
+
+</head>
+<body>
+
+</body>
+</html>
